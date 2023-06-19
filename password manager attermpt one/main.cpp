21879963f78c75b30aa606f03b4665479c03b5ce
Noah Branley <112705279+NoahBranley@users.noreply.github.com>
2023-06-19 20:06:40 +0800

    Add project files.

---
+++ password manager attermpt one/main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int i;
+string in0;
+int pin;
+int stall;
+string websites[4] = { "github.com", "youtube.com", "twitch.tv", "google.com" };
+int main() {
+										for (string i : websites) {
+										cout << i <<"\n";}
+										cout << "--------------\n>"; // basic ui
+										cin >> in0;
+	if (in0 == "github"){
+		system("cls");
+		cout << "enter pin....\n>";
+		cin >> pin;
+		if (pin == 5555) {
+			system("cls");
+			cout << "2$g3@!$$%@#FGH433";
+			cin >> stall;
+		}
+		else {
+			system("cls");
+			cout << "ACCESS DENIED........!";
+			cin >> stall;
+		}
+	
+	}                //github pass
+}                    //main statement                 
