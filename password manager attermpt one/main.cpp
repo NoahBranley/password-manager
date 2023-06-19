849fe5d1e740882ab69da049f025d28088304c8e
Noah Branley <112705279+NoahBranley@users.noreply.github.com>
2023-06-19 20:37:11 +0800

    added the other websites from the array

--- password manager attermpt one/main.cpp
+++ password manager attermpt one/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int i;
 int pin;
 string in0; //this is not actually my password or used pins! this is currently a proof of concept and helping me learn arrays!
-int stall;
+int stall; // this is just so it looks better lol
 string websites[4] = { "github.com", "youtube.com", "twitch.tv", "google.com" };
 int main() {
 										for (string i : websites) {
@@ -24,6 +24,53 @@ int main() {
 			cout << "ACCESS DENIED........!";
 			cin >> stall;
 		}
-	
 	}                //github pass
-}                    //main statement                 
+
+	if (in0 == "youtube") {
+		system("cls");
+		cout << "enter pin....\n>";
+		cin >> pin;
+		if (pin == 5555) {
+			system("cls");
+			cout << "fgdkS@#$sdedj4789@d";
+			cin >> stall;
+		}
+		else {
+			system("cls");
+			cout << "ACCESS DENIED........!";
+			cin >> stall;
+		}
+	}			//youtube pass
+
+	if (in0 == "twitch") {
+		system("cls");
+		cout << "enter pin....\n>";
+		cin >> pin;
+		if (pin == 5555) {
+			system("cls");
+			cout << "wzTG692&l&p@";
+			cin >> stall;
+		}
+		else {
+			system("cls");
+			cout << "ACCESS DENIED........!";
+			cin >> stall;
+		}
+	}                //twich pass
+
+	if (in0 == "google") {
+		system("cls");
+		cout << "enter pin....\n>";
+		cin >> pin;
+		if (pin == 5555) {
+			system("cls");
+			cout << "@L&fyzI9s01p";
+			cin >> stall;
+		}
+		else {
+			system("cls");
+			cout << "ACCESS DENIED........!";
+			cin >> stall;
+		}
+	}			//google pass
+}                    //main statement              
