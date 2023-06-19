6a37e600e6316de6d59e1c1354534aac00bf3b20
Noah Branley <112705279+NoahBranley@users.noreply.github.com>
2023-06-19 20:10:57 +0800

    accidental deletion of the pin variable

--- password manager attermpt one/main.cpp
+++ password manager attermpt one/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int i;
+int pin;
 string in0; //this is not actually my password or used pins! this is currently a proof of concept and helping me learn arrays!
 int stall;
 string websites[4] = { "github.com", "youtube.com", "twitch.tv", "google.com" };
