56dd1809f66baeb8e6a5791b46f7341db1539ea6
NoahBranley <taken.me.king@gmail.comm>
2023-06-20 13:29:58 +0800

    added a way to go back to the menu. and some ui changes

--- password manager attermpt one/main.cpp
+++ password manager attermpt one/main.cpp
@@ -6,77 +6,81 @@ string in0; //this is not actually my password or used pins! this is currently a
 int stall; // this is just so it looks better lol
 string websites[4] = { "github.com", "youtube.com", "twitch.tv", "google.com" };
 int main() {
-										for (string i : websites) {
-										cout << i <<"\n";}
-										cout << "--------------\n>"; // basic ui stuff balls
-										cin >> in0;
-	if (in0 == "github"){
-		system("cls");
-		cout << "enter pin....\n>";
-		cin >> pin;
-		if (pin == 5555) {
-			system("cls");
-			cout << "2$g3@!$$%@#FGH433";
-			cin >> stall;
+	while (stall == 0) {
+		cout << "noahs password manager.... (^<>^).... \ninput 0 to return to this menu at any time....\n--------------\n";
+		for (string i : websites) {
+			cout << i << "\n";
 		}
-		else {
+		cout << "--------------\n>"; // basic ui stuff
+		cin >> in0;
+		if (in0 == "github") {
 			system("cls");
-			cout << "ACCESS DENIED........!";
-			cin >> stall;
-		}
-	}                //github pass
+			cout << "enter pin....\n>";
+			cin >> pin;
+			if (pin == 5555) {
+				system("cls");
+				cout << "2$g3@!$$%@#FGH433";
+				cin >> stall;
+			}
+			else {
+				system("cls");
+				cout << "ACCESS DENIED........!";
+				cin >> stall;
+			}
+		}                //github pass
 
-	if (in0 == "youtube") {
-		system("cls");
-		cout << "enter pin....\n>";
-		cin >> pin;
-		if (pin == 5555) {
+		if (in0 == "youtube") {
 			system("cls");
-			cout << "fgdkS@#$sdedj4789@d";
-			cin >> stall;
-		}
-		else {
-			system("cls");
-			cout << "ACCESS DENIED........!";
-			cin >> stall;
-		}
-	}			//youtube pass
+			cout << "enter pin....\n>";
+			cin >> pin;
+			if (pin == 5555) {
+				system("cls");
+				cout << "fgdkS@#$sdedj4789@d";
+				cin >> stall;
+			}
+			else {
+				system("cls");
+				cout << "ACCESS DENIED........!";
+				cin >> stall;
+			}
+		}			//youtube pass
 
-	if (in0 == "twitch") {
-		system("cls");
-		cout << "enter pin....\n>";
-		cin >> pin;
-		if (pin == 5555) {
+		if (in0 == "twitch") {
 			system("cls");
-			cout << "wzTG692&l&p@";
-			cin >> stall;
-		}
-		else {
-			system("cls");
-			cout << "ACCESS DENIED........!";
-			cin >> stall;
-		}
-	}                //twich pass
+			cout << "enter pin....\n>";
+			cin >> pin;
+			if (pin == 5555) {
+				system("cls");
+				cout << "wzTG692&l&p@";
+				cin >> stall;
+			}
+			else {
+				system("cls");
+				cout << "ACCESS DENIED........!";
+				cin >> stall;
+			}
+		}                //twich pass
 
-	if (in0 == "google") {
-		system("cls");
-		cout << "enter pin....\n>";
-		cin >> pin;
-		if (pin == 5555) {
+		if (in0 == "google") {
 			system("cls");
-			cout << "@L&fyzI9s01p";
-			cin >> stall;
-		}
-		else {
-			system("cls");
-			cout << "ACCESS DENIED........!";
-			cin >> stall;
-		}
-	}			//google pass
+			cout << "enter pin....\n>";
+			cin >> pin;
+			if (pin == 5555) {
+				system("cls");
+				cout << "@L&fyzI9s01p";
+				cin >> stall;
+			}
+			else {
+				system("cls");
+				cout << "ACCESS DENIED........!";
+				cin >> stall;
+			}
+		}			//google pass
 
-	else {
-		cout << "not a valid webiste....";
-		cin >> stall;
+		else {
+			cout << "not a valid webiste....";
+			cin >> stall;
+		}
+		system("cls");
 	}
-
 }                    //main statement              
