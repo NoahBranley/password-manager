8cf62a2939f1bc153d566e797f7ec2c725f75e07
Noah Branley <112705279+NoahBranley@users.noreply.github.com>
2023-06-26 17:56:17 +0800

    Major overhaul and cleanup

--- password manager attermpt one/main.cpp
+++ password manager attermpt one/main.cpp
@@ -1,86 +1,34 @@
+//this is not actually my password or used pins! this is currently a proof of concept and helping me learn arrays!
 #include<iostream>
+#include<map>
+#include<string>
 using namespace std;
-int i;
-int pin;
-string in0; //this is not actually my password or used pins! this is currently a proof of concept and helping me learn arrays!
-int stall; // this is just so it looks better lol
 string websites[4] = { "github.com", "youtube.com", "twitch.tv", "google.com" };
 int main() {
-	while (stall == 0) {
-		cout << "noahs password manager.... (^<>^).... \ninput 0 to return to this menu at any time....\n--------------\n";
-		for (string i : websites) {
-			cout << i << "\n";
-		}
-		cout << "--------------\n>"; // basic ui stuff
-		cin >> in0;
-		if (in0 == "github") {
-			system("cls");
-			cout << "enter pin....\n>";
-			cin >> pin;
-			if (pin == 5555) {
-				system("cls");
-				cout << "2$g3@!$$%@#FGH433";
-				cin >> stall;
-			}
-			else {
-				system("cls");
-				cout << "ACCESS DENIED........!";
-				cin >> stall;
-			}
-		}                //github pass
-
-		if (in0 == "youtube") {
-			system("cls");
-			cout << "enter pin....\n>";
-			cin >> pin;
-			if (pin == 5555) {
-				system("cls");
-				cout << "fgdkS@#$sdedj4789@d";
-				cin >> stall;
-			}
-			else {
-				system("cls");
-				cout << "ACCESS DENIED........!";
-				cin >> stall;
-			}
-		}			//youtube pass
-
-		if (in0 == "twitch") {
-			system("cls");
-			cout << "enter pin....\n>";
-			cin >> pin;
-			if (pin == 5555) {
-				system("cls");
-				cout << "wzTG692&l&p@";
-				cin >> stall;
-			}
-			else {
-				system("cls");
-				cout << "ACCESS DENIED........!";
-				cin >> stall;
-			}
-		}                //twich pass
-
-		if (in0 == "google") {
-			system("cls");
-			cout << "enter pin....\n>";
-			cin >> pin;
-			if (pin == 5555) {
-				system("cls");
-				cout << "@L&fyzI9s01p";
-				cin >> stall;
-			}
-			else {
-				system("cls");
-				cout << "ACCESS DENIED........!";
-				cin >> stall;
-			}
-		}			//google pass
-
-		else {
-			cout << "not a valid webiste....";
-			cin >> stall;
-		}
-		system("cls");
-	}
-}                    //main statement              
+    map<string, string> input_map{
+        {"github", "2$g3@!$$%@#FGH433"},
+        {"youtube","fgdkS@#$sdedj4789@d"},
+        {"twitch", "wzTG692&l&p@"},
+        {"google", "@L&fyzI9s01p"}};
+    while(1){
+    cout << "noahs password manager.... (^<>^).... \n";
+    cout<< "--------------\n";
+    for (string i : websites) {
+    cout << i << "\n";}
+    cout << "--------------\n>"; // basic ui stuff
+    string input_1;
+    cin >> input_1;
+    int pin;
+    if (input_map.contains(input_1)) {
+            system("cls");
+            cout << "enter pin...\n>";
+            cin >> pin;
+    if (pin == 5555) {
+            system("cls");
+            cout << input_map.at(input_1);
+            string a; cin >> a;}
+    else { cout << "ACCESS DENEID"; }}
+    else { cout << "not a valid website.."; }
+    system("cls");
+}
+}//main statement              
